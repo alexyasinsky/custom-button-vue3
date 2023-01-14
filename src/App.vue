b9bfe2cbdc2423378f9f4e8a90d3fdd73fb1c421
alexyasinsky <alex_yasinsky@mail.ru>
2023-01-14 17:15:58 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import CustomButton from "./components/CustomButton.vue";
+  const sayHello = () => alert('hello');
+  const title = 'helloBtn'
+</script>
+
+<template>
+  <CustomButton :title='title' :handler="sayHello"/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <button type="button" @click="handler">{{ title }}</button>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: 'Untitled'
+      },
+      handler: {
+        type: Function,
+        default: ()=> {}
+      }
+    },
+
+    data() {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+<style scoped lang="scss">
+
+</style>
